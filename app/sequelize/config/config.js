2c4ecb760bc7d475b19ea7445cdbbbf6ea6e9d86
rhenandias <diasrhenan@gmail.com>
2022-04-09 15:29:46 -0300

    Corrigindo configuração do banco de dados para produção

--- app/sequelize/config/config.js
+++ app/sequelize/config/config.js
@@ -22,10 +22,18 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST_NAME,
     dialect: "postgres",
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
