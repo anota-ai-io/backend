46291b49ee89084fc0502fd1ff438681d5221a78
Rhenan <diasrhenan@gmail.com>
2022-05-06 10:22:50 -0300

    Alterando ordem de criação de modelos

--- app/sequelize/models/init-models.js
+++ app/sequelize/models/init-models.js
@@ -8,34 +8,54 @@ var _refreshToken = require("./refreshToken");
 var _user = require("./user");
 
 function initModels(sequelize) {
-  var comment = _comment(sequelize, DataTypes);
+  var user = _user(sequelize, DataTypes);
+  var refreshToken = _refreshToken(sequelize, DataTypes);
   var hashtag = _hashtag(sequelize, DataTypes);
   var post = _post(sequelize, DataTypes);
+  var comment = _comment(sequelize, DataTypes);
   var postHashtag = _postHashtag(sequelize, DataTypes);
   var postLike = _postLike(sequelize, DataTypes);
-  var refreshToken = _refreshToken(sequelize, DataTypes);
-  var user = _user(sequelize, DataTypes);
 
-  hashtag.belongsToMany(post, { as: 'postId_posts', through: postHashtag, foreignKey: "hashtagId", otherKey: "postId" });
-  post.belongsToMany(hashtag, { as: 'hashtagId_hashtags', through: postHashtag, foreignKey: "postId", otherKey: "hashtagId" });
-  post.belongsToMany(user, { as: 'userId_users', through: postLike, foreignKey: "postId", otherKey: "userId" });
-  user.belongsToMany(post, { as: 'postId_post_postLikes', through: postLike, foreignKey: "userId", otherKey: "postId" });
-  postHashtag.belongsTo(hashtag, { as: "hashtag", foreignKey: "hashtagId"});
-  hashtag.hasMany(postHashtag, { as: "postHashtags", foreignKey: "hashtagId"});
-  comment.belongsTo(post, { as: "post", foreignKey: "postId"});
-  post.hasMany(comment, { as: "comments", foreignKey: "postId"});
-  postHashtag.belongsTo(post, { as: "post", foreignKey: "postId"});
-  post.hasMany(postHashtag, { as: "postHashtags", foreignKey: "postId"});
-  postLike.belongsTo(post, { as: "post", foreignKey: "postId"});
-  post.hasMany(postLike, { as: "postLikes", foreignKey: "postId"});
-  comment.belongsTo(user, { as: "user", foreignKey: "userId"});
-  user.hasMany(comment, { as: "comments", foreignKey: "userId"});
-  post.belongsTo(user, { as: "user", foreignKey: "userId"});
-  user.hasMany(post, { as: "posts", foreignKey: "userId"});
-  postLike.belongsTo(user, { as: "user", foreignKey: "userId"});
-  user.hasMany(postLike, { as: "postLikes", foreignKey: "userId"});
-  refreshToken.belongsTo(user, { as: "user", foreignKey: "userId"});
-  user.hasMany(refreshToken, { as: "refreshTokens", foreignKey: "userId"});
+  hashtag.belongsToMany(post, {
+    as: "postId_posts",
+    through: postHashtag,
+    foreignKey: "hashtagId",
+    otherKey: "postId",
+  });
+  post.belongsToMany(hashtag, {
+    as: "hashtagId_hashtags",
+    through: postHashtag,
+    foreignKey: "postId",
+    otherKey: "hashtagId",
+  });
+  post.belongsToMany(user, {
+    as: "userId_users",
+    through: postLike,
+    foreignKey: "postId",
+    otherKey: "userId",
+  });
+  user.belongsToMany(post, {
+    as: "postId_post_postLikes",
+    through: postLike,
+    foreignKey: "userId",
+    otherKey: "postId",
+  });
+  postHashtag.belongsTo(hashtag, { as: "hashtag", foreignKey: "hashtagId" });
+  hashtag.hasMany(postHashtag, { as: "postHashtags", foreignKey: "hashtagId" });
+  comment.belongsTo(post, { as: "post", foreignKey: "postId" });
+  post.hasMany(comment, { as: "comments", foreignKey: "postId" });
+  postHashtag.belongsTo(post, { as: "post", foreignKey: "postId" });
+  post.hasMany(postHashtag, { as: "postHashtags", foreignKey: "postId" });
+  postLike.belongsTo(post, { as: "post", foreignKey: "postId" });
+  post.hasMany(postLike, { as: "postLikes", foreignKey: "postId" });
+  comment.belongsTo(user, { as: "user", foreignKey: "userId" });
+  user.hasMany(comment, { as: "comments", foreignKey: "userId" });
+  post.belongsTo(user, { as: "user", foreignKey: "userId" });
+  user.hasMany(post, { as: "posts", foreignKey: "userId" });
+  postLike.belongsTo(user, { as: "user", foreignKey: "userId" });
+  user.hasMany(postLike, { as: "postLikes", foreignKey: "userId" });
+  refreshToken.belongsTo(user, { as: "user", foreignKey: "userId" });
+  user.hasMany(refreshToken, { as: "refreshTokens", foreignKey: "userId" });
 
   return {
     comment,
